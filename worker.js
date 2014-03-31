0d6d2ad001120c657d3c447a8d8f2363a8bd674a
Michael Narayan <mnarayan01@gmail.com>
2014-03-31 17:16:29 -0400

    Wait a short period of time before doing exhaustive search in the worker.

--- worker.js
+++ worker.js
@@ -3,13 +3,13 @@ var _ = require('underscore');
 var Analyst = require('./src/analyst.js');
 var ExploratoryUtilities = require('./src/exploratory-utilities.js');
 
-var started = false;
+var workerStartedAt = null;
 
 process.on('message', function (params) {
-  if(started) {
+  if(workerStartedAt) {
     throw new Error();
   } else {
-    started = true;
+    workerStartedAt = Date.now();
 
     run(params);
   }
@@ -18,22 +18,16 @@ process.on('message', function (params) {
 function run(params) {
   var graph = new Analyst.PatheryGraph(params.board);
   var currBlocks = graph.dictify_blocks(params.initialSolution);
-  var topAnnealingScore = null;
-  var topTotalScore = null;
+  var topScore = null;
 
   while(true) {
     var annealingResult = Analyst.annealingIteration(graph, currBlocks);
+    var exhaustiveSearchResult = exhaustiveSearchWrapper(annealingResult.score, currBlocks, graph);
 
-    if(topAnnealingScore === null || annealingResult.score > topAnnealingScore) {
-      topAnnealingScore = annealingResult.score;
+    if(topScore === null || exhaustiveSearchResult.score > topScore) {
+      topScore = exhaustiveSearchResult.score;
 
-      var exhaustiveSearchResult = exhaustiveSearchWrapper(annealingResult.score, currBlocks, graph);
-
-      if(topTotalScore === null || exhaustiveSearchResult.score > topTotalScore) {
-        topTotalScore = exhaustiveSearchResult.score;
-
-        process.send(exhaustiveSearchResult);
-      }
+      process.send(exhaustiveSearchResult);
     }
   }
 }
@@ -41,29 +35,96 @@ function run(params) {
 ////////////////////////////////////////////////////////////////////////////////
 // Exhaustive search functionality.
 
+/**
+ * @function
+ *
+ * @param {Number} currAnnealingScore
+ * @param {Object} currBlocks
+ * @param {Analyst.PatheryGraph} graph
+ * @returns {{score: Number, solution: Object}}
+ */
 var exhaustiveSearchWrapper;
+
+/** @variable {Number */
 var exhaustiveSearchDepth = ExploratoryUtilities.configuration.exhaustiveSearchDepth;
 
 if(exhaustiveSearchDepth > 0) {
   // TODO: Timeout the exhaustive search after some period of time?
   // TODO:     Additionally -- lower the depth if too many timeouts?
 
-  exhaustiveSearchWrapper = function (score, currBlocks, graph) {
-    // console.log('Worker ' + process.pid + ' started exhaustive search on ' + score + '.');
+  /**
+   * The number of milliseconds to wait after the worker starts before attempting exhaustive searches.
+   *
+   * @see delayExhaustiveSearch
+   *
+   * @constant {Number} - Wait this number of milliseconds
+   */
+  const DELAY_EXHAUSTIVE_SEARCH_FOR_MILLISECONDS = 2000;
 
-    var startTime = Date.now();
-    var res = performExhaustiveSearch(_.extend({}, currBlocks), graph);
-    var runTime = Date.now() - startTime;
+  /**
+   * This will be set to false once @see DELAY_EXHAUSTIVE_SEARCH_FOR_MILLISECONDS have passed, thus causing exhaustive
+   * searches to go forward.
+   *
+   * @variable {boolean}
+   */
+  var delayExhaustiveSearch = true;
 
-    console.log('Worker ' + process.pid + ' finished exhaustive search on score yielding ' + res.score + ' after ' + (runTime / 1000) + ' seconds.');
+  /**
+   * The top score we have seen solely looking at the annealing process. This _does not_ include scores generated via
+   * exhaustive searching.
+   *
+   * @variable {Number}
+   */
+  var topAnnealingScore = null;
 
+  exhaustiveSearchWrapper = function (currAnnealingScore, currBlocks, graph) {
+    if(delayExhaustiveSearch) {
+      delayExhaustiveSearch = (Date.now() - workerStartedAt) < DELAY_EXHAUSTIVE_SEARCH_FOR_MILLISECONDS;
+    } else {
+      var doExhaustiveSearch;
+
+      if(topAnnealingScore === null || currAnnealingScore > topAnnealingScore) {
+        topAnnealingScore = currAnnealingScore;
+
+        doExhaustiveSearch = true;
+      } else {
+        doExhaustiveSearch = false;
+      }
+
+      if(doExhaustiveSearch) {
+        // console.log('Worker ' + process.pid + ' started exhaustive search on ' + currAnnealingScore + '.');
+
+        var startTime = Date.now();
+        var res = performExhaustiveSearch(_.extend({}, currBlocks), graph);
+        var runTime = Date.now() - startTime;
+
+        console.log('Worker ' + process.pid + ' finished exhaustive search on ' + currAnnealingScore + ' yielding ' + res.score + ' after ' + (runTime / 1000) + ' seconds.');
+
+        if(res.score > currAnnealingScore) {
+          return {
+            score: res.score,
+            solution: graph.dictify_blocks(res.solution)
+          }
+        }
+      }
+    }
+
+    // Simply return the input if we did not attempt an exhaustive or the exhaustive did not yield an improved score.
     return {
-      score: res.score,
-      solution: graph.dictify_blocks(res.solution)
+      score: currAnnealingScore,
+      solution: currBlocks
     }
   };
 
+  /**
+   * @function
+   *
+   * @param {Object} currBlocks
+   * @param {Analyst.PatheryGraph} graph
+   * @returns {{score: Number, solution: Number[][] }}
+   */
   var performExhaustiveSearch;
+
   switch(ExploratoryUtilities.configuration.exhaustiveSearchDomain) {
     case 'combinatorial':
       performExhaustiveSearch = function(currBlocks, graph) {
