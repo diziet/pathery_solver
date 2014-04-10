b0259351ad2bd8ad08ac8c168cf749781530f0c3
Michael Narayan <mnarayan01@gmail.com>
2014-04-10 11:28:46 -0400

    Add examples to test/find-path.js.

--- test/find-path.js
+++ test/find-path.js
@@ -1,3 +1,20 @@
+/******************************************************************************
+  Examples:
+
+  1.  [Map 4534](http://www.pathery.com:80/scores#2014-03-31_4534_1_) - Simple. Score should be 16.
+
+          MAP_ID=4534 \
+            BLOCKS='[[0,10],[1,5],[1,10],[2,4],[3,4],[4,3],[5,2]]' \
+            node test/find-path.js
+
+  2.  [Map 4578](http://www.pathery.com:80/scores#2014-04-10_4578_1_) - Dualing paths. Score should be 240.
+
+          MAP_ID=4578 \
+            BLOCKS='[[1,7],[1,9],[3,5],[3,11],[4,6],[4,12],[5,4],[5,13],[5,16],[6,9],[7,6],[7,14],[8,4]]' \
+            node test/find-path.js
+
+ ******************************************************************************/
+
 var Analyst = require(__dirname + '/../src/analyst.js');
 var PatheryAPI = require(__dirname + '/../src/communication/api.js');
 
